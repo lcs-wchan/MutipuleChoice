478c129c16fe6bbbd9598abb651c4989e4ee3c76
WilliamChan <wchan@lcs.on.ca>
2019-12-03 15:05:11 -0500

    Initial Commit

---
+++ MutipuleChoice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MutipuleChoice
+//
+//  Created by William Chan on 2019-12-03.
+//  Copyright © 2019 WilliamChan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
